d6ae984445bbf0558f7a886a6afef336f6cceba9
Igor Santiago <igor.santiago546@outlook.com>
2023-08-22 18:22:00 -0300

    Inicio do projeto

---
+++ menu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">Logotipo</a>
+    <!--Botão para habilitar as opções do menu -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+    <!--Fim do botão-->
+</nav>
+</body>
+</html>
